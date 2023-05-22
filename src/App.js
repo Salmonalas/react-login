db22dbb780e3f2b9b30a587b2f91393f19664204
Salmonalas <92855591+Salmonalas@users.noreply.github.com>
2023-05-22 14:24:54 +0700

    edit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import Login from './Login';
 import Register from './Register';
 import Profile from './Profile';
 import { Route, Routes } from 'react-router-dom';
+import Navbar from './component/Navbar/Navbar';
+import './App.css';
 
 function App() {
   return (
+    <>
+    <Navbar/>
     <Routes>
       <Route path = "/login" element = {<Login />} />
       <Route path = "/register" element = {<Register />} />
       <Route path = "/profile" element = {<Profile />} />
     </Routes>
+    </>
   );
 }
 
